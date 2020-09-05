c7d09c289231bbc964970f438bcaefd637ec2839
biswal19 <chandrikabiswal1@gmail.com>
2020-09-05 04:54:09 -0700

    Updates

---
+++ precache-manifest.6fec49e0ec0d48ee0c8cd2a03cc8b43f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c13d7f4ae78589d3fe577d82af531183",
+    "url": "/Counter-app-react/index.html"
+  },
+  {
+    "revision": "c1b8735cb6d988b236ac",
+    "url": "/Counter-app-react/static/css/main.1030a6fc.chunk.css"
+  },
+  {
+    "revision": "631e961ecc420674d2cf",
+    "url": "/Counter-app-react/static/js/2.d9093ec4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Counter-app-react/static/js/2.d9093ec4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1b8735cb6d988b236ac",
+    "url": "/Counter-app-react/static/js/main.03b118eb.chunk.js"
+  },
+  {
+    "revision": "b69ef353cee6699ccef4",
+    "url": "/Counter-app-react/static/js/runtime-main.7200606a.js"
+  }
+]);
